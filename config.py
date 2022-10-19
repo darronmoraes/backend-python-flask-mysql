44e8e3d6fc5bdcfc158c849f8fda6d22788361c2
Darron Tacio Moraes <111967762+darronmoraes@users.noreply.github.com>
2022-10-19 11:41:56 +0530

    changed host to rds endpoint config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from flaskext.mysql import MySQL
 # MYSQL object
 mysql = MySQL()
 
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_HOST'] = 'rds-numino.czuxz43ipobf.ap-south-1.rds.amazonaws.com'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
 app.config['MYSQL_DATABASE_DB'] = 'numino'
